722472aedcfc901b3035cc93c573adb041f0c6bb
thefinnlab <finnlabneuro@gmail.com>
2021-02-27 13:24:39 -0500

    attempt to fix a bbug to save debrief data on MTurk- attempt6

--- src/App.svelte
+++ src/App.svelte
@@ -5,53 +5,47 @@
 -->
 
 <script>
-    	import { db, auth, serverTime, params, ratingTypes, dev,
+    import { db, auth, serverTime, params, ratingTypes, dev,
             experiment, userGroup, labName, email} from './utils.js';
-
-    	import { onMount } from 'svelte';
+    
+    import { onMount } from 'svelte';
 	import Intro from './pages/Intro.svelte';
 	import Botcheck from './pages/Botcheck.svelte';
 	import Consent from './pages/Consent.svelte';
 	import Instructions1 from './pages/Instructions1.svelte';
 	import Instructions2 from './pages/Instructions2.svelte';
-    	import Demo from './pages/Demo.svelte';
+    import Demo from './pages/Demo.svelte';
 	import Task from './pages/Task.svelte';
 	import Debrief from './pages/Debrief.svelte';
-    	import Complete from './pages/Complete.svelte';
-    	import Loading from './components/Loading.svelte';
-    	import Header from './components/Header.svelte';
-    	import MTurkPreview from './pages/MTurkPreview.svelte';
+    import Complete from './pages/Complete.svelte';
+    import Loading from './components/Loading.svelte';
+    import Header from './components/Header.svelte';
+    import MTurkPreview from './pages/MTurkPreview.svelte';
 
-    // path details
-    	const ratingsPath = `${experiment}/ratings`;
-    	const ratingsDoc = db.doc(ratingsPath);
-    	const subjectGroupPath = `${experiment}/subjects/${userGroup}`;
-    	const subjectGroupCollection = db.collection(subjectGroupPath);
-    	const stimuliPath = `${experiment}/stimuli`;
-    	const stimuliDoc = db.doc(stimuliPath);
-    	const demo_stimuliPath = `${experiment}/demo_stimuli`;
-    	const demo_stimuliDoc = db.doc(demo_stimuliPath);
+	// path details
+	const ratingsPath = `${experiment}/ratings`;
+	const ratingsDoc = db.doc(ratingsPath);
+	const subjectGroupPath = `${experiment}/subjects/${userGroup}`;
+    const subjectGroupCollection = db.collection(subjectGroupPath);
+    const stimuliPath = `${experiment}/stimuli`;
+    const stimuliDoc = db.doc(stimuliPath);
 
-    // declare and set other necessary variables
+	// declare and set other necessary variables
 	let currVid;
 	let currVidSrc;
-	let demo_currVid;
-	let demo_currVidSrc;
 	let currRating;
 	let subjectPath;
 	let ratingDocPathway;
 	let currentState;
-    	let consentStatus;
-    	let alreadyWatched = [];
-    	let moviesRemaining = [];
-	let demo_moviesRemaining = [];
-    	let numOptions;
-    	let time = 0;
-    	let initExperiment = false;
-
+    let consentStatus;
+    let alreadyWatched = [];
+    let moviesRemaining = [];
+    let numOptions;
+    let time = 0;
+    let initExperiment = false;
+    
     // use to validate build type in JS console
     console.log(dev);
-    console.log('tester boiii');
 
     const resetTestWorker = async () => {
         // Change to the new state within Svelte
@@ -65,8 +59,8 @@
                         // deletes all previous ratings
                         ratingList.forEach(function(doc) {
                             console.log('deleting: ', doc.id);
-                            db.collection(currPath).doc(doc.id).delete()
-                        });
+                            db.collection(currPath).doc(doc.id).delete()   
+                        });      
                         // updates subject log
                         subjectRef.update({
                             startTime: serverTime,
@@ -82,16 +76,16 @@
             console.log(`Reset user requested but workerId is ${params.workerId}`);
         }
     };
-
-
-
-
+    
+    
+    
+	
 	// *****************************
 	// main function
     // *****************************
 
-	// Before we render anything see if we have a db entry for this subject based upon the URL parameters. If not
-	// create an entry with a new random stimulus order and put them into the instructions state.
+	// Before we render anything see if we have a db entry for this subject based upon the URL parameters. If not 
+	// create an entry with a new random stimulus order and put them into the instructions state. 
     // If we do, load their trial order and current experiment state
 
     if (params.assignmentId === 'ASSIGNMENT_ID_NOT_AVAILABLE') {
@@ -105,7 +99,7 @@
     } else {
         currentState = 'non-mturk';
     }
-
+	
     onMount(async () => { // right when DOM is created
         if (initExperiment) {
             try {
@@ -119,7 +113,7 @@
                             console.log('user found...signing in with credentials');
                             // then look for document
                         } catch (error) {
-                            if (error.code == 'auth/user-not-found') {
+                            if (error.code === 'auth/user-not-found') {
                                 console.log('no user found...creating new credentials');
                                 // if login fails, create new user
                                 await auth.createUserWithEmailAndPassword(
@@ -135,35 +129,20 @@
                         console.log('user authenticated...');
                         let currUser = auth.currentUser;
                         try { // if user already signed in, grab relevant document
-												let subjectRef = subjectGroupCollection.doc(params.workerId);
-												subjectPath = `${subjectGroupPath}/${params.workerId}`; // setting for use in HTML below
-												subjectRef.get().then(function(doc) {
-												console.log(doc)
-												if (doc.exists) { // load old document
-                                 console.log('previous document found...loading state...');
-																 let data = doc.data();
-																 //console.log(data)
-														    subjectGroupCollection.doc(params.workerId + '_' + serverTime).set(data)
-															// updates most recent login time
-                                subjectRef.update({
-                                    startTime: serverTime,
-																		consentStatus: 'incomplete',
-																});
-																//subjectRef.set({
-																//				name: 'unknown',
-                                //        workerId: params.workerId,
-                                //        assignmentId: params.assignmentId,
-                                //        hitId: params.hitId,
-                                //        userId: currUser.uid,
-                                //        startTime: serverTime,
-                                //        consentStatus: 'incomplete'
-                                //    });
-                          } else { // create a new document
-																//subjectGroupCollection.doc(params.workerId).set({name: 'unknown'});
-																console.log('no previous documents found...creating new...');
-																subjectPath = `${subjectGroupPath}/${params.workerId}`; // setting for use in HTML below
-																subjectRef.set({
-																				name: 'unknown',
+                            let subjectRef = subjectGroupCollection.doc(params.workerId);
+                            subjectPath = `${subjectGroupPath}/${params.workerId}`; // setting for use in HTML below
+                            subjectRef.get().then(function(doc) {
+                                if (doc.exists) { // load old document
+                                    console.log('previous document found...loading state...');
+                                    // updates most recent login time
+                                    subjectRef.update({
+                                        mostRecentTime: serverTime
+                                    });
+                                } else { // create a new document
+                                    subjectGroupCollection.doc(params.workerId).set({name: 'unknown'});
+                                    console.log('no previous documents found...creating new...');
+                                    subjectPath = `${subjectGroupPath}/${params.workerId}`; // setting for use in HTML below
+                                    subjectRef.set({
                                         workerId: params.workerId,
                                         assignmentId: params.assignmentId,
                                         hitId: params.hitId,
@@ -171,86 +150,42 @@
                                         startTime: serverTime,
                                         consentStatus: 'incomplete'
                                     });
-                          }
-                          // grab stimuli doc and add all movies to list
-                          stimuliDoc.get().then(function(stimuliTable) {
-                          for (var field in stimuliTable.data()) {
-                              moviesRemaining.push(field);
-                          }
-                          // check to see which movies subject has already viewed (if any)
-                          let currPath = `${ratingsPath}/${params.workerId}`;
-
-                          db.collection(currPath).get().then(function(ratingList) {
-                          // removes already completed movies from option set
-															console.log('rating list')
-															console.log(ratingList)
-                              ratingList.forEach(function(doc) {
-              										moviesRemaining = removeItemOnce(moviesRemaining, doc.id.split("-")[0]);
-                              });
-                              // see how many movies are left
-                              numOptions = moviesRemaining.length;
-                              console.log('moviesRemaining: ', moviesRemaining);
-                              // if any movie-rating pairings left, load and start
-                              if (numOptions > 0) {
-                              // choose random movie and rating type
-                                            let movieIndex = Math.floor(Math.random()*moviesRemaining.length);
-                                            let ratingIndex = Math.floor(Math.random()*ratingTypes.length); // always 0 for us (since ratingTypes = ['social])
-                                            console.log('ratingTypes.length:',ratingTypes.length)
-
-					  																currVid = moviesRemaining[movieIndex];
-					  																currRating = ratingTypes[ratingIndex]; // 'social' in our case
-                                            let vidPlusRating = `${currVid}-${currRating}`;//videoname-social
-                                            ratingDocPathway = `${ratingsPath}/${params.workerId}/${vidPlusRating}`;
-                                            // grab URL for video sourcing
-                                            currVidSrc = stimuliTable.data()[currVid];
-																						//subjectRef.update({vid: currVid});
-					  																console.log('url:',currVidSrc)
-                                            updateState('consent');
-
-                                        } else {
-                                            console.log("no options left!");
-                                            updateState('complete');
-                                        }
-                                    });
-                                });
-
-								// DEMO PAGE - grab stimuli doc and add all movies to list
-                                demo_stimuliDoc.get().then(function(demo_stimuliTable) {
-                                    for (var field in demo_stimuliTable.data()) {
-                                        demo_moviesRemaining.push(field);
+                                }
+                                // grab stimuli doc and add all movies to list
+                                stimuliDoc.get().then(function(stimuliTable) {
+                                    for (var field in stimuliTable.data()) {
+                                        moviesRemaining.push(field);         
                                     }
                                     // check to see which movies subject has already viewed (if any)
                                     let currPath = `${ratingsPath}/${params.workerId}`;
                                     db.collection(currPath).get().then(function(ratingList) {
                                         // removes already completed movies from option set
                                         ratingList.forEach(function(doc) {
-                                            demo_moviesRemaining = removeItemOnce(demo_moviesRemaining, doc.id.split("-")[0]);
+                                            moviesRemaining = removeItemOnce(moviesRemaining, doc.id.split("-")[0]);
                                         });
                                         // see how many movies are left
-                                        numOptions = demo_moviesRemaining.length;
-                                        console.log('demo_moviesRemaining: ', demo_moviesRemaining);
+                                        numOptions = moviesRemaining.length;
+                                        console.log('moviesRemaining: ', moviesRemaining);
                                         // if any movie-rating pairings left, load and start
                                         if (numOptions > 0) {
                                         // choose random movie and rating type
-                                            let demo_movieIndex = Math.floor(Math.random()*demo_moviesRemaining.length);
-                                            //let ratingIndex = Math.floor(Math.random()*ratingTypes.length);
-                                            demo_currVid = demo_moviesRemaining[demo_movieIndex];
-											console.log('demo_currVid:',demo_currVid)
-											//currRating = ratingTypes[ratingIndex];
-                                            //let vidPlusRating = `${currVid}-${currRating}`;
-                                            //ratingDocPathway = `${ratingsPath}/${params.workerId}/${vidPlusRating}`;
-                                            // grab URL for video sourcing
-                                            demo_currVidSrc = demo_stimuliTable.data()[demo_currVid];
-											console.log('demo url:',demo_currVidSrc)
-                                            updateState('consent'); // REMOVE THIS?
-
+                                            let movieIndex = Math.floor(Math.random()*moviesRemaining.length);
+                                            let ratingIndex = Math.floor(Math.random()*ratingTypes.length);
+                                            currVid = moviesRemaining[movieIndex];
+                                            currRating = ratingTypes[ratingIndex];
+                                            let vidPlusRating = `${currVid}-${currRating}`;
+                                            ratingDocPathway = `${ratingsPath}/${params.workerId}/${vidPlusRating}`;
+                                            // grab URL for video sourcing 
+                                            currVidSrc = stimuliTable.data()[currVid];
+                                            updateState('consent');
+                                            
                                         } else {
                                             console.log("no options left!");
                                             updateState('complete');
                                         }
                                     });
                                 });
-                            });
+                            });	
                         } catch (error) {
                             console.error(error);
                         }
@@ -266,19 +201,16 @@
 	// handler functions
 	// *****************************
 
-	// this function updates the current state of the user to
+	// this function updates the current state of the user to 
 	// dynamically render different parts of the experiment (i.e. instructions, quiz, etc)
   	const updateState = async (newState) => {
     	// Change to the new state within Svelte
 		currentState = newState;
 		try {
-
 			await db.doc(`${experiment}/subjects/${userGroup}/${params.workerId}`).update({
 				currentState
 			});
-
 			console.log('updated user state');
-
 		} catch (error) {
 			console.error(error);
 		}
@@ -348,7 +280,7 @@
     </div>
 	{#if !currentState}
         <Loading>Loading...</Loading>
-    	{:else if currentState === 'mturk-preview'}
+    {:else if currentState === 'mturk-preview'}
         <MTurkPreview />
 	{:else if currentState === 'intro'}
 		<Intro on:finished={() => updateState('consent')}></Intro>
@@ -361,26 +293,19 @@
 	{:else if currentState === 'instructions1'}
 		<Instructions1 ratingType={currRating} numOptions={numOptions} on:finished={() => updateState('demo')} />
 	{:else if currentState === 'demo'}
-		<Demo
-			src={demo_currVidSrc}
-			time={time}
-			ratingType={currRating}
-			on:back={() => updateState('instructions1')}
-			on:finished={() => updateState('instructions2')}
-			 />
+		<Demo time={time} ratingType={currRating} on:back={() => updateState('instructions1')} on:finished={() => updateState('instructions2')} />
 	{:else if currentState === 'instructions2'}
 		<Instructions2 on:back={() => updateState('demo')} on:finished={() => updateState('task')} />
 	{:else if currentState === 'task'}
-		<Task
+		<Task 
 			src={currVidSrc}
 			ratingType={currRating}
-			time={time}
-			pathway={ratingDocPathway}
-			on:finished={() => updateState('debrief')}
+			time={time} 
+			pathway={ratingDocPathway} 
+			on:finished={() => updateState('debrief')} 
 		></Task>
 	{:else if currentState === 'debrief'}
 		<Debrief
-			vid={currVidSrc}
 			subPath={subjectPath}
 			email={email}
 			labName={labName}
@@ -388,5 +313,9 @@
 		></Debrief>
 	{:else if currentState === 'complete'}
 		<Complete></Complete>
-	{/if}
+	{/if}  	 
 </div>
+
+
+
+
